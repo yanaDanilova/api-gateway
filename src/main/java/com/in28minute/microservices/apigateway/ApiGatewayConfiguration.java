285ff002ba1da4925c3b4012f614719cd3a49bbe
yanad <yana.danilova1509@gmail.com>
2023-09-01 13:34:32 +0200

    last step

--- src/main/java/com/in28minute/microservices/apigateway/ApiGatewayConfiguration.java
+++ src/main/java/com/in28minute/microservices/apigateway/ApiGatewayConfiguration.java
@@ -14,15 +14,27 @@ import java.util.function.Predicate;
 @Configuration
 public class ApiGatewayConfiguration {
 
-    @Bean
+/*    @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(p -> p.path("/get")
-                        .filters(f -> f.addRequestHeader("MyHeader", "MyURI")
+                .route(p -> p
+                        .path("/get")
+         *//**//*               .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
                                 .addRequestParameter("Param", "MyValue"))
                         .uri("http://httpbin.org:80"))
-                .route(p->p.path("/currency-exchange/**")
+                .route(p -> p.path("/currency-exchange/**")
                         .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath(
+                                "/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion"))
                 .build();
-    }
+    }*/
+
 }
